b0514d309e29674a789c4cdea924a02f89826908
schen126 <sec022@ucsd.edu>
2023-01-30 19:10:58 -0800

    Update StringServer.java

--- StringServer.java
+++ StringServer.java
@@ -4,21 +4,20 @@ import java.net.URI;
 class Handler implements URLHandler {
     // The one bit of state on the server: a number that will be manipulated by
     // various requests.
-    int num = 0;
+    int numStrings = 0;
+    String messages = "";
 
     public String handleRequest(URI url) {
         if (url.getPath().equals("/")) {
-            return String.format("Serena's Number: %d", num);
-        } else if (url.getPath().equals("/increment")) {
-            num += 1;
-            return String.format("Number incremented!");
+            return String.format("You've entered %d messages so far!", numStrings);
         } else {
-            System.out.println("Path: " + url.getPath());
-            if (url.getPath().contains("/add")) {
+            //System.out.println("Path: " + url.getPath());
+            if (url.getPath().contains("/add-message")) {
                 String[] parameters = url.getQuery().split("=");
-                if (parameters[0].equals("count")) {
-                    num += Integer.parseInt(parameters[1]);
-                    return String.format("Number increased by %s! It's now %d", parameters[1], num);
+                if (parameters[0].equals("s")) {
+                    messages += "\n" + parameters[1];
+                    numStrings+=1;
+                    return messages;
                 }
             }
             return "404 Not Found!";
@@ -26,7 +25,7 @@ class Handler implements URLHandler {
     }
 }
 
-class NumberServer {
+class StringServer {
     public static void main(String[] args) throws IOException {
         if(args.length == 0){
             System.out.println("Missing port number! Try any number between 1024 to 49151");
